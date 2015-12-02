6ccb214094924b86abdc0515f936a165db72f9de
MacKinley Smith <smit1625@msu.edu>
2015-12-02 13:17:38 -0700

    Added runner class, added new options

--- lib/guard/sunspot.rb
+++ lib/guard/sunspot.rb
@@ -1,29 +1,50 @@
 require 'guard/compat/plugin'
-require "guard/sunspot/version"
+require 'guard/sunspot/version'
 
 module Guard
   class Sunspot < Plugin
+    attr_reader :options, :runner
+
+    DEFAULT_OPTIONS = {
+      environment: 'development',
+      bundler: false,
+      zeus: false,
+      start_on_start: true,
+      timeout: 30
+    }
+
     def initialize(options={})
       super
-
-      @environment = options.fetch(:environment, 'development')
-      @bundler = options.fetch(:bundler, false)
-      @zeus = options.fetch(:zeus, false)
+      @options = DEFAULT_OPTIONS.merge(options)
+      @runner = Runner.new(@options)
+      # @environment = @options.fetch(:environment, 'development')
+      # @bundler = @options.fetch(:bundler, false)
     end
 
     def start
-      system "#{rake} sunspot:solr:start RAILS_ENV=#{@environment}"
-      UI.info "Sunspot started"
+      UI.info "[Guard::Sunspot] will start Solr in #{options[:environment]}."
+      reload('start') if options[:start_on_start]
+      # system "#{rake} sunspot:solr:start RAILS_ENV=#{@environment}"
+      # UI.info "Sunspot started"
     end
 
     def stop
-      system "#{rake} sunspot:solr:stop RAILS_ENV=#{@environment}"
-      UI.info "Sunspot stopped"
+      UI.info "[Guard::Sunspot] will stop Solr in #{options[:environment]}."
+      runner.stop
+      # system "#{rake} sunspot:solr:stop RAILS_ENV=#{@environment}"
+      # UI.info "Sunspot stopped"
     end
 
-    def reload
-      stop
-      start
+    def reload(action='restart')
+      title = "#{action.capitalize}ing Solr..."
+      UI.info title
+      if runner.restart
+        UI.info "Solr #{action}ed, pid #{runner.pid}"
+      else
+        UI.info "Solr NOT #{action}ed, check your log files."
+      end
+      # stop
+      # start
     end
 
     def run_on_changes(path)
@@ -32,14 +53,14 @@ module Guard
     # for guard 1.0.x and earlier
     alias :run_on_change :run_on_changes
 
-    private
-
-    def rake
-      rake = []
-      rake << "bundle exec" if @bundler
-      rake << "zeus" if @zeus
-      rake << "rake"
-      rake.join ' '
-    end
+    # private
+    #
+    # def rake
+    #   rake = []
+    #   rake << "bundle exec" if @bundler
+    #   rake << "zeus" if @zeus
+    #   rake << "rake"
+    #   rake.join ' '
+    # end
   end
 end
---
+++ lib/guard/sunspot/runner.rb
@@ -0,0 +1,94 @@
+module Guard
+  class Sunspot
+    class Runner
+      MAX_WAIT_COUNT = 10
+
+      attr_reader :options, :pid
+
+      def initialize(options)
+        @options = options
+        @root = options[:root] ? File.expand_path(options[:root]) : Dir.pwd
+      end
+
+      def start
+        if options[:zeus] && !wait_for_zeus
+          UI.info "[Guard::Sunspot::Error] Could not find zeus socket file."
+          return false
+        end
+        run_sunspot_command! 'start'
+        wait_for_pid
+      end
+
+      def stop
+        return unless has_pid?
+        run_sunspot_command! 'stop'
+        wait_for_no_pid
+      end
+
+      def restart; stop; start end
+
+      def environment
+        { 'RAILS_ENV' => options[:zeus] ? nil : options[:environment] }
+      end
+
+      def pid_file
+        @pid_file ||= File.expand_path( Rails.root, 'solr', 'pids',
+          options[:environment], "sunspot-solr-#{options[:environment]}.pid" )
+      end
+
+      def pid; has_pid? ? read_pid : nil end
+
+      def sleep_time
+        options[:timeout].to_f / MAX_WAIT_COUNT.to_f
+      end
+
+      def wait_for_zeus
+        wait_for_loop { File.exist? zeus_sockfile }
+      end
+
+      private
+
+      def build_rake_command
+        cmd = []
+        cmd << 'bundle exec' if @bundler
+        cmd << 'zeus' if @zeus
+        cmd << 'rake'
+        cmd.join ' '
+      end
+
+      def build_solr_command(cmd)
+        "#{build_rake_command} sunspot:solr:#{cmd}"
+      end
+
+      def run_sunspot_command!(cmd)
+        if options[:zeus]
+          without_bundler_env { system environment, build_solr_command(cmd) }
+        else system environment, build_solr_command(cmd) end
+      end
+
+      def without_bundler_env
+        if defined?(::Bundler)
+          ::Bundler.with_clean_env { yield }
+        else yield end
+      end
+
+      def has_pid?; File.file? pid_file end
+      def read_pid; Integer(File.read(pid_file)); rescue ArgumentError; nil end
+
+      def wait_for_loop
+        count = 0
+        while !yield && count < MAX_WAIT_COUNT
+          wait_for_action
+          count += 1
+        end
+        !(count == MAX_WAIT_COUNT)
+      end
+      def wait_for_action; sleep sleep_time end
+      def wait_for_pid;    wait_for_loop {  has_pid? } end
+      def wait_for_no_pid; wait_for_loop { !has_pid? } end
+
+      def zeus_sockfile; File.join(@root, '.zeus.sock') end
+
+    end
+  end
+end
